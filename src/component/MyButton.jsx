9d814da4f84054fbdee40d010b285f80d1b33615
ALFIO BATTIATO <battiato3@gmail.com>
2024-04-03 00:05:26 +0200

    profilo finito alfio

--- src/component/MyButton.jsx
+++ src/component/MyButton.jsx
@@ -5,7 +5,7 @@ function MyButton(props) {
     return (
 
         <Link to={props.link}>
-            <Button className='rounded-pill fw-semibold' variant={`outline-${props.colore}`}
+            <Button className='rounded-pill fw-semibold' variant={`${props.colore}`}
                 onClick={props.funzione}
                 type={props.type}>
                 <div className='d-flex'> {props.img && (<img src={`${props.img}`} alt='img'></img>)}{props.text}</div>
