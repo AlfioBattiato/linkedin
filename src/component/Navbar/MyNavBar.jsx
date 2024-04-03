4e2779b4486ebb6aedd4b3495d430c109f20d0be
Mandi Thomaz <118818675+MandiMarchetti@users.noreply.github.com>
2024-04-03 21:05:24 +0200

    Working on navbar. Changes were made on MyNavBar, I added a new svg called 'tre_punti' on asstes folder, and the file navbar.css contains also changes that I couldn't do using only rect bootstrap

--- src/component/Navbar/MyNavBar.jsx
+++ src/component/Navbar/MyNavBar.jsx
@@ -9,36 +9,40 @@ import { Link } from "react-router-dom";
 
 function MyNavBar() {
   return (
-    <Navbar collapseOnSelect expand="sm" className="flex-xs-row-reverse bg-body-tertiary">
-      <Container className="justify-content-evenly">
+    <Navbar collapseOnSelect expand="xs" className="flex-xs-row-reverse bg-body-tertiary">
+      <Container className="justify-content-evenly justify-md-content-between">
         <div className="d-flex align-items-center gap-sm-3">
           <div className="d-flex me-2">
             <Navbar.Brand href="#home" className="container_logo_navbar_fixed m-0" width="45px" height="45px">
               <img src="/assets/logo.svg" alt="logo" width="45px" height="45px" className="Logo_navbar_fixed " />
             </Navbar.Brand>
           </div>
-
-          <div>
-            <div className="position-relative">
-              <img src="/assets/cerca_bold.svg" alt="cerca_bold" className="d-lg-none" style={{ width: "30px" }} />
-              <img
-                src="/assets/cerca.svg"
-                alt="research_icon"
-                className="d-none d-lg-block position-absolute research_icon_navbar"
-              />
-              <Form.Control
-                type="text"
-                placeholder="Cerca"
-                className="d-none d-lg-block ps-5 research_input_form_navbar"
-                width={"20%"}
-              />
-            </div>
-          </div>
         </div>
 
-        <div className="d-md-flex">
+        <div className="d-flex">
           <div className=" gap-5 d-flex ">
-            <Nav className="flex-xs-row-reverse d-md-flex align-items-center ms-4 gap-4">
+            <Nav className="d-flex flex-row align-items-center ms-3 ms-md-4 gap-4">
+              <div>
+                <div className="position-relative">
+                  <img
+                    src="/assets/cerca_bold.svg"
+                    alt="cerca_bold"
+                    className="d-lg-none cerca_bold_icon_navbar"
+                    style={{ width: "25px" }}
+                  />
+                  <img
+                    src="/assets/cerca.svg"
+                    alt="research_icon"
+                    className="d-none d-lg-block position-absolute research_icon_navbar"
+                  />
+                  <Form.Control
+                    type="text"
+                    placeholder="Cerca"
+                    className="d-none d-lg-block ps-5 research_input_form_navbar"
+                    width={"20%"}
+                  />
+                </div>
+              </div>
               <div className="icons_navbar">
                 <Nav.Link href="/Home" className="p-0">
                   <img src="/assets/home.svg" alt="home_icon" />
@@ -72,13 +76,46 @@ function MyNavBar() {
                   <p className="d-none d-md-block m-0">Notifications</p>
                 </Nav.Link>
               </div>
+              <div className="icons_navbar icon_profile_navbar">
+                <Nav.Link href="#" className="p-0 ">
+                  <img
+                    src="https://images.unsplash.com/photo-1600486913747-55e5470d6f40?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTl8fG1hbnxlbnwwfHwwfHx8MA%3D%3D"
+                    alt="profile_picture"
+                    width={"25px"}
+                    height={"25px"}
+                    className="image_dropdown_navbar object-fit-cover"
+                  />
+                  <p className="d-none d-md-block m-0">Me</p>
+                </Nav.Link>
+              </div>
+              <div className="icons_navbar icon_business_navbar">
+                <Nav.Link href="#" className="p-0">
+                  <img src="/assets/business.svg" alt="business_icon mb-0" />
+                  <p className="d-none d-md-block m-0">Business</p>
+                </Nav.Link>
+              </div>
+              <div className="icons_navbar icon_tre_punti_navbar">
+                <Nav.Link href="#" className="p-0 position-relative">
+                  <img src="/assets/tre_punti.svg" alt="business_icon mb-0" />
+                  <NavDropdown
+                    id="basic-nav-dropdown"
+                    className="position-absolute"
+                    style={{ right: 0, left: 0, top: 0, bottom: 0 }}
+                  >
+                    <Nav.Link href="#" className="p-0">
+                      <img src="/assets/business.svg" alt="business_icon mb-0" />
+                      <p className="d-none d-md-block m-0">Business</p>
+                    </Nav.Link>
+                  </NavDropdown>
+                </Nav.Link>
+              </div>
             </Nav>
           </div>
 
-          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-          <Navbar.Collapse id="responsive-navbar-nav" className="justify-content-start gap-2 ms-4">
+          {/* <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse expand="xs" id="responsive-navbar-nav" className="justify-content-start gap-2 ms-4">
             <div className="icons_navbar">
-              <div className="d-flex justify-content-center ">
+              <div className="d-flex justify-content-center">
                 <img
                   src="https://images.unsplash.com/photo-1600486913747-55e5470d6f40?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTl8fG1hbnxlbnwwfHwwfHx8MA%3D%3D"
                   alt="profile_picture"
@@ -87,39 +124,56 @@ function MyNavBar() {
                   className="image_dropdown_navbar object-fit-cover"
                 />
               </div>
-
-              <NavDropdown title="Me" id="collapsible-nav-dropdown" className="p-0 ">
-                <div style={{ width: "18rem" }}>
-                  <div className="px-2">
-                    <img
-                      src="https://images.unsplash.com/photo-1600486913747-55e5470d6f40?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTl8fG1hbnxlbnwwfHwwfHx8MA%3D%3D"
-                      alt="draft_picture"
-                      width="30px"
-                      height={"30px"}
-                      className="image_dropdown_navbar object-fit-cover"
-                    />
+              <NavDropdown>
+                <div style={{ width: "20rem" }}>
+                  <div className="d-flex px-2">
+                    <div className="d-flex" style={{ width: "52px" }}>
+                      <img
+                        src="https://images.unsplash.com/photo-1600486913747-55e5470d6f40?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTl8fG1hbnxlbnwwfHwwfHx8MA%3D%3D"
+                        alt="draft_picture"
+                        width={"50px"}
+                        height={"50px"}
+                        className="image_dropdown_navbar object-fit-cover ms-1"
+                      />
+                    </div>
                     <div>
-                      <p className="fw-semibold">Amanda Marchetti</p>
-                      <p>Full Stack Developer</p>
-
-                      <div>
-                        <Link to={"/Profilo"}>
-                          <MyButton text={"Visualizza Profilo"} colore={"primary"}></MyButton>
-                        </Link>
-                        <Link to={"/Login"}>
-                          <MyButton text={"Uscire"} colore={"primary"}></MyButton>
-                        </Link>
+                      <div className="ms-2">
+                        <p className="fw-semibold mb-0">Mario Rossi</p>
+                        <p>Full Stack Developer</p>
                       </div>
                     </div>
                   </div>
+                  <div className="mx-3">
+                    <Link to={"/Profilo"} className="d-flex justify-content-center" style={{ width: "18rem" }}>
+                      <MyButton
+                        text={"Visualizza Profilo"}
+                        colore={"primary"}
+                        link={"/Profilo"}
+                        className="text-center"
+                        Style={{ width: "100%!important" }}
+                      ></MyButton>
+                    </Link>
+                  </div>
                 </div>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
               </NavDropdown>
             </div>
 
+            <div className="icons_navbar">
+              <NavDropdown title="Me" className="dropdown-backdrop p-0 "></NavDropdown>
+              <div className="mx-3">
+                <Link to={"/Login"} className="d-flex justify-content-center mt-3" style={{ width: "18rem" }}>
+                  <MyButton
+                    text={"Uscire"}
+                    colore={"primary"}
+                    link={"/Login"}
+                    className="text-center"
+                    Style={{ width: "100%!important" }}
+                  ></MyButton>
+                </Link>
+              </div>
+            </div>
             <Nav>
-              <div className="container icons_navbar">
+              <div className="container icons_navbar icon_business_navbar">
                 <div className="d-flex justify-content-center"></div>
                 <img src="/assets/business.svg" alt="business_icon mb-0" />
                 <NavDropdown title="Business" id="collapsible-nav-dropdown">
@@ -135,7 +189,7 @@ function MyNavBar() {
                 </NavDropdown>
               </div>
             </Nav>
-          </Navbar.Collapse>
+          </Navbar.Collapse> */}
         </div>
       </Container>
     </Navbar>
