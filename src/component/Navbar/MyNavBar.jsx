497410a269a0aa136595bcef749e01022e8e9e4e
Mandi Thomaz <118818675+MandiMarchetti@users.noreply.github.com>
2024-04-04 11:14:14 +0200

    commit - Navbar 100% responsive for all screens from 238px forward. It's still missing the Dropdown that I'm working right now

--- src/component/Navbar/MyNavBar.jsx
+++ src/component/Navbar/MyNavBar.jsx
@@ -13,7 +13,7 @@ function MyNavBar() {
       <Container className="justify-content-evenly">
         <div className="d-flex align-items-center gap-sm-3">
           <div className="d-flex justify-content-md-end">
-            <Navbar.Brand href="#home" className="container_logo_navbar_fixed m-0" width="45px" height="45px">
+            <Navbar.Brand href="/Profilo" className="container_logo_navbar_fixed m-0" width="45px" height="45px">
               <img src="/assets/logo.svg" alt="logo" width="45px" height="45px" className="Logo_navbar_fixed " />
             </Navbar.Brand>
           </div>
@@ -43,39 +43,64 @@ function MyNavBar() {
                   />
                 </div>
               </div>
+
+              {/* ////////////////////////////////////LINK HOME ////////////////////////////////////////// */}
               <div className="icons_navbar">
                 <Nav.Link href="/Home" className="p-0 icon_home_navebar">
-                  <img src="/assets/home.svg" alt="home_icon" />
+                  <img src="/assets/home.svg" alt="home_icon" id="home_icon_nav" />
                   <p className="d-none d-lg-block m-0"> Home</p>
                 </Nav.Link>
               </div>
+              {/* /////////FINO LINK HOME ////////////////////////////////////////// */}
 
+              {/* ////////////////////////////////////LINK NETWORK ////////////////////////////////////////// */}
               <div className="icons_navbar">
                 <Nav.Link href="/Rete" className="p-0">
                   <img src="/assets/people.svg" alt="network_icon" />
                   <p className="d-none d-lg-block m-0">Network</p>
                 </Nav.Link>
               </div>
+              {/* ///////FINO LINK NETWORK ////////////////////////////////////////////////////////////////// */}
 
-              <div className="icons_navbar">
-                <Nav.Link href="/Lavoro" className="p-0">
+              {/* ////////////////////////////////////LINK JOBS ////////////////////////////////////////// */}
+              <div className="icons_navbar icon_jobs_navbar">
+                <Nav.Link href="/Lavoro" className="p-0 ">
                   <img src="/assets/work.svg" alt="jobs_icon" />
                   <p className="d-none d-lg-block m-0">Jobs</p>
                 </Nav.Link>
               </div>
+              {/* ///////FINO LINK JOBS ////////////////////////////////////////////////////////////////// */}
 
-              <div className="icons_navbar">
+              {/* ////////////////////////////////////LINK MESSAGES ////////////////////////////////////////// */}
+              <div className="icons_navbar icon_messages_navbar">
                 <Nav.Link href="/Messagistica" className="p-0">
                   <img src="/assets/message.svg" alt="messages_icon" />
                   <p className="d-none d-lg-block m-0">Messages</p>
                 </Nav.Link>
               </div>
-              <div className="icons_navbar">
+              {/* ///FINO LINK MESSAGES ////////////////////////////////////////////////////////////////////// */}
+
+              {/* ////////////////////////////////////LINK NOTIFICATIONS ////////////////////////////////////////// */}
+              <div className="icons_navbar icon_bell_navbar">
                 <Nav.Link href="/Notifiche" className="p-0">
                   <img src="/assets/campana.svg" alt="notification_icon" />
                   <p className="d-none d-lg-block m-0">Notifications</p>
+                  <NavDropdown
+                    id="basic-nav-dropdown"
+                    className="position-absolute"
+                    style={{ right: 0, left: 0, top: 0, bottom: 0 }}
+                  >
+                    <NavDropdown.Item href="#">
+                      <div className="d-flex justify-content-center">
+                        <img src="/assets/campana.svg" alt="notification_icon" className="bell_dropdown_navbar" />
+                      </div>
+                    </NavDropdown.Item>
+                  </NavDropdown>
                 </Nav.Link>
               </div>
+              {/* ///////FINO LINK NOTIFICATIONS//////////////////////////////////////////////////////////// */}
+
+              {/* ////////////////////////////////LINK PROFILO//////////////////////////////////////////////// */}
               <div className="icons_navbar icon_profile_navbar">
                 <Nav.Link href="#" className="p-0 ">
                   <img
@@ -105,24 +130,40 @@ function MyNavBar() {
                   </NavDropdown>
                 </Nav.Link>
               </div>
+              {/* /////////FIONO LINK PROFILO///////////////////////////////////////////////////////////// */}
+
+              {/* ///////////////////////////////////////LINK BUSINESS///////////////////////////////////////////// */}
               <div className="icons_navbar icon_business_navbar">
                 <Nav.Link href="#" className="p-0">
                   <img src="/assets/business.svg" alt="business_icon mb-0" />
                   <p className="d-none d-lg-block m-0">Business</p>
                 </Nav.Link>
               </div>
+              {/* /////////FIONO LINK BUSINESS///////////////////////////////////////////////////////////// */}
+
+              {/* /////////////////////////INIZIO ICONA TRE PUNTINI//////////////////////////////////////////////////////// */}
+
               <div className="icons_navbar ">
                 <Nav.Link href="#" className="p-0 position-relative">
                   <img src="/assets/tre_punti.svg" alt="business_icon mb-0" className="icon_tre_punti_navbar" />
                   <NavDropdown
                     id="basic-nav-dropdown"
-                    className="position-absolute"
+                    className="position-absolute dropdown_icon_tre_punti_navbar"
                     style={{ right: 0, left: 0, top: 0, bottom: 0 }}
                   >
-                    <Nav.Link href="#" className="p-0">
+                    {/* ///////DROPDOWN LINK MESSAGES/////////// */}
+                    <Nav.Link href="/Messagistica" className="p-0 icon_messages_dropdown_navbar">
+                      <img src="/assets/message.svg" alt="messages_icon" />
+                      <p className="d-none d-lg-block m-0">Messages</p>
+                    </Nav.Link>
+
+                    {/* ///////DROPDOWN LINK BUSINESS////////// */}
+                    <Nav.Link href="#" className="p-0 icon_business_dropdown_navbar">
                       <img src="/assets/business.svg" alt="business_icon mb-0" />
                       <p className="d-none d-lg-block m-0">Business</p>
                     </Nav.Link>
+
+                    {/* ////////////DROPDOWN LINK PROFILO//////// */}
                     <Nav.Link href="#" className="icon_profile_dropdown_navbar">
                       <img
                         src="https://images.unsplash.com/photo-1600486913747-55e5470d6f40?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTl8fG1hbnxlbnwwfHwwfHx8MA%3D%3D"
@@ -133,11 +174,22 @@ function MyNavBar() {
                       />
                       <p className="d-none d-lg-block m-0">Me</p>
                     </Nav.Link>
+                    {/* //////////DROPDOWN LINK NOTIFICATIONS/////////// */}
+                    <Nav.Link href="/Notifiche" className="p-0 icon_bell_dropdown_navbar">
+                      <img src="/assets/campana.svg" alt="notification_icon" />
+                      <p className="d-none d-lg-block m-0">Notifications</p>
+                    </Nav.Link>
+                    {/* //////////DROPDOWN LINK JOBS/////////// */}
+                    <Nav.Link href="/Lavoro" className="p-0 icon_jobs_dropdown_navbar">
+                      <img src="/assets/work.svg" alt="jobs_icon" />
+                      <p className="d-none d-lg-block m-0">Jobs</p>
+                    </Nav.Link>
                   </NavDropdown>
                 </Nav.Link>
               </div>
             </Nav>
           </div>
+          {/* /////////FIONO ICONA TRE PUNTINI///////////////////////////////////////////////////////////// */}
 
           {/* <Navbar.Toggle aria-controls="responsive-navbar-nav" />
           <Navbar.Collapse expand="xs" id="responsive-navbar-nav" className="justify-content-start gap-2 ms-4">
