62868d6cc73928086ca2473e9d4beb2174fbdfa7
ALFIO BATTIATO <battiato3@gmail.com>
2024-04-04 22:06:15 +0200

    ok

--- src/component/Navbar/MyNavBar.jsx
+++ src/component/Navbar/MyNavBar.jsx
@@ -2,11 +2,13 @@ import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import "./navbar.css";
-import { Form, Dropdown } from "react-bootstrap";
-import MyButton from "../MyButton";
+import { Form, Dropdown, Button, } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 function MyNavBar() {
+ 
+
   return (
     // {/* ////////////////////////////////////NAVBAR BEGINNING ////////////////////////////////////////// */}
     <Navbar collapseOnSelect expand="xs" className="flex-xs-row-reverse bg-body-tertiary">
@@ -52,7 +54,6 @@ function MyNavBar() {
                     style={{ width: "80%", paddingLeft: "1.8rem" }}
                   />
                 </div>
-
                 {/* ////////////////////////////////////LINK HOME ////////////////////////////////////////// */}
                 <div className="icons_navbar">
                   <Nav.Link href="/Home" className="p-0 icon_home_navebar">
@@ -60,9 +61,7 @@ function MyNavBar() {
                     <p className="d-none d-md-block m-0"> Home</p>
                   </Nav.Link>
                 </div>
-                {/* /////////FINO LINK HOME ////////////////////////////////////////// */}
-
-                {/* ////////////////////////////////////LINK NETWORK ////////////////////////////////////////// */}
+                {/* /////////FINO LINK HOME ///////////////////////////////////////////////////////////////// */}
                 <div className="icons_navbar">
                   <Nav.Link href="/Rete" className="p-0">
                     <img src="/assets/people.svg" alt="network_icon" />
@@ -70,8 +69,6 @@ function MyNavBar() {
                   </Nav.Link>
                 </div>
                 {/* ///////FINO LINK NETWORK ////////////////////////////////////////////////////////////////// */}
-
-                {/* ////////////////////////////////////LINK JOBS ////////////////////////////////////////// */}
                 <div className="icons_navbar icon_jobs_navbar">
                   <Nav.Link href="/Lavoro" className="p-0 ">
                     <img src="/assets/work.svg" alt="jobs_icon" />
@@ -79,8 +76,6 @@ function MyNavBar() {
                   </Nav.Link>
                 </div>
                 {/* ///////FINO LINK JOBS ////////////////////////////////////////////////////////////////// */}
-
-                {/* ////////////////////////////////////LINK MESSAGES ////////////////////////////////////////// */}
                 <div className="icons_navbar icon_messages_navbar">
                   <Nav.Link href="/Messagistica" className="p-0">
                     <img src="/assets/message.svg" alt="messages_icon" />
@@ -88,8 +83,6 @@ function MyNavBar() {
                   </Nav.Link>
                 </div>
                 {/* ///FINO LINK MESSAGES ////////////////////////////////////////////////////////////////////// */}
-
-                {/* ////////////////////////////////////LINK NOTIFICATIONS ////////////////////////////////////////// */}
                 <div className="icons_navbar icon_bell_navbar">
                   <Nav.Link href="/Notifiche" className="p-0">
                     <img src="/assets/campana.svg" alt="notification_icon" />
@@ -97,131 +90,70 @@ function MyNavBar() {
                   </Nav.Link>
                 </div>
                 {/* ///////FINO LINK NOTIFICATIONS//////////////////////////////////////////////////////////// */}
-
-                {/* ////////////////////////////////LINK PROFILO//////////////////////////////////////////////// */}
                 <div className="icons_navbar icon_profile_navbar">
-                  <Nav.Link href="#" className="p-0 ">
-                    <img
-                      src="https://images.unsplash.com/photo-1600486913747-55e5470d6f40?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTl8fG1hbnxlbnwwfHwwfHx8MA%3D%3D"
-                      alt="profile_picture"
-                      width={"25px"}
-                      height={"25px"}
-                      className="image_dropdown_navbar object-fit-cover"
-                    />
-
-                    {/* /////DROPDOWN PROFILE BEGINNING//// */}
-                    <Dropdown>
-                      <Dropdown.Toggle
-                        variant="light"
-                        id="dropdown-basic"
-                        className="position-relative d-none d-md-block m-0 toggle_dropdown_navbar"
-                        style={{ color: "gray", height: "25px" }}
-                      >
-                        Me
-                      </Dropdown.Toggle>
-
-                      {/* //////////////BOX PROFILE//////////////// */}
-                      <Dropdown.Menu
-                        className="position-absolute"
-                        style={{ right: 10, left: -255, top: 24, bottom: 0, height: "26rem", width: "18rem" }}
-                      >
-                        <div style={{ width: "18rem" }}>
-                          <div className="d-flex px-1 py-1 justify-content-start align-items-center">
-                            <div className="d-flex " style={{ width: "52px" }}>
-                              <img
-                                src="https://images.unsplash.com/photo-1600486913747-55e5470d6f40?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTl8fG1hbnxlbnwwfHwwfHx8MA%3D%3D"
-                                alt="draft_picture"
-                                width={"60px"}
-                                height={"60px"}
-                                className="image_dropdown_navbar object-fit-cover ms-1"
-                              />
+                  <img
+                    src="https://images.unsplash.com/photo-1600486913747-55e5470d6f40?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTl8fG1hbnxlbnwwfHwwfHx8MA%3D%3D"
+                    alt="profile_picture"
+                    width={"25px"}
+                    height={"25px"}
+                    className="image_dropdown_navbar object-fit-cover"
+                  />
+                  {/* /////DROPDOWN PROFILE BEGINNING//// */}
+                  <Dropdown>
+                    <Dropdown.Toggle
+                     id="dropdown-basic"
+                    //  onClick={toggleDropdown}
+                      variant="light"
+                      className="position-relative d-none d-md-block m-0 toggle_dropdown_navbar"
+                      style={{ color: "gray", height: "25px" }}
+                    >
+                      Me
+                    </Dropdown.Toggle>
+                    <Dropdown.Menu
+                     className={`position-absolute`}
+                      style={{ right: 10, left: -255, top: 24, bottom: 0, height: "23rem", width: "18rem" }}
+                    >
+                      <div style={{ width: "18rem" }}>
+                        <div className="d-flex px-1 py-1 justify-content-start align-items-center ">
+                          <div className="d-flex " style={{ width: "52px" }}>
+                            <img
+                              src="https://images.unsplash.com/photo-1600486913747-55e5470d6f40?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTl8fG1hbnxlbnwwfHwwfHx8MA%3D%3D"
+                              alt="draft_picture"
+                              width={"60px"}
+                              height={"60px"}
+                              className="image_dropdown_navbar object-fit-cover ms-1"
+                            />
+                          </div>
+                          <div className="ps-4">
+                            <div>
+                              <p className="fw-semibold mb-0">Mario Rossi</p>
+                              <p className="mb-0">Full Stack Developer</p>
                             </div>
-                            <div className="ps-4">
-                              <div>
-                                <p className="fw-semibold mb-0">Mario Rossi</p>
-                                <p className="mb-0">Full Stack Developer</p>
-                              </div>
-                            </div>
-                          </div>
-                          <div className="mx-4 mt-2 ">
-                            <div className="d-flex justify-content-center" style={{ width: "16rem" }}>
-                              <MyButton
-                                text={"Visualizza Profilo"}
-                                colore={"primary"}
-                                // link={"/Profilo"}
-                                className="text-center"
-                                Style={{ width: "16rem" }}
-                              ></MyButton>
-                            </div>
-                          </div>
-                          <div
-                            className="empty_div_line_dropdown_profile mt-2 mx-2"
-                            style={{ width: "17rem", backgroundColor: "gray", height: "0.1rem" }}
-                          ></div>
-                          <div className="mx-4 mt-2">
-                            <h6>Conta</h6>
-                            <p className="my-1">Privacy Configuration</p>
-                            <p className="my-1">Help</p>
-                            <p className="my-1">Language</p>
-                          </div>
-
-                          <div
-                            className="empty_div_line_dropdown_profile mt-2 mx-2"
-                            style={{ width: "17rem", backgroundColor: "gray", height: "0.1rem" }}
-                          ></div>
-
-                          <div className="mx-4 mt-2">
-                            <h6>Settings</h6>
-                            <p className="my-1">Publications and Activities</p>
-                            <p className="my-1">Account to announce jobs</p>
-                          </div>
-
-                          <div
-                            className="empty_div_line_dropdown_profile mt-2 mx-2"
-                            style={{ width: "17rem", backgroundColor: "gray", height: "0.1rem" }}
-                          ></div>
-
-                          <div className="mx-4 mt-2 ">
-                            {/* <Link to={"/Profilo"} className="d-flex justify-content-center" style={{ width: "16rem" }}>
-                              <MyButton
-                                text={"Exit"}
-                                colore={"primary"}
-                                link={"/Profilo"}
-                                className="text-center pb-2"
-                                Style={{ width: "16rem" }}
-                              ></MyButton>
-                            </Link> */}
                           </div>
                         </div>
-                      </Dropdown.Menu>
-                    </Dropdown>
+                        <div className="px-4 border-bottom pb-2">
+                        <Link to={"/Profilo"} >
+                          <Button variant="outline-primary" className="sizeSmall rounded-pill fw-semibold py-1 w-100" >Visualizza profilo</Button>
+                         </Link>
+                        </div>
+                        <div className="px-4 border-bottom">
+                          <h6>Account</h6>
+                          <p className="my-1  text-secondary">Privacy Configuration</p>
+                          <p className="my-1  text-secondary">Help</p>
+                          <p className="my-1  text-secondary">Language</p>
+                        </div>
 
-
-
-
-                    
-                    {/* /////FINO DROPDOWN PROFILE BEGINNING//// */}
-                    {/* /////DROPDOWN PROFILE RESPONSIVE//// */}
-                    {/* <Dropdown className="position-relative">
-                      <Dropdown.Toggle
-                        // href="/Profilo"
-                        variant="light"
-                        id="dropdown-basic"
-                        className="position-absolute d-lg-none m-0 toggle_dropdown_navbar"
-                        style={{
-                          color: "gray",
-                          width: "25px",
-                          height: "25px",
-                          borderRadius: "50%",
-                          opacity: "0.0",
-                          right: 0,
-                          left: 0,
-                          top: -23,
-                          bottom: 0,
-                        }}
-                      ></Dropdown.Toggle>
-                    </Dropdown> */}
-                  </Nav.Link>
+                        <div className="px-4 border-bottom pt-2">
+                          <h6>Settings</h6>
+                          <p className="my-1  text-secondary">Publications and Activities</p>
+                          <p className="my-1  text-secondary">Account to announce jobs</p>
+                        </div>
+                        <div className="px-4  pt-2">
+                          <Link to={"/"} className="text-decoration-none text-secondary">Exit</Link>
+                        </div>
+                      </div>
+                    </Dropdown.Menu>
+                  </Dropdown>
                 </div>
                 {/* /////////FIONO LINK PROFILO///////////////////////////////////////////////////////////// */}
                 <div className="icons_navbar icon_business_navbar">
@@ -239,9 +171,7 @@ function MyNavBar() {
                     </Dropdown>
                   </Nav.Link>
                 </div>
-
                 {/* /////////FIONO LINK BUSINESS///////////////////////////////////////////////////////////// */}
-
                 {/* /////////////////////////INIZIO ICONA TRE PUNTINI//////////////////////////////////////////////////////// */}
 
                 <div className="icons_navbar ">
