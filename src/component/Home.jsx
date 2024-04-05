d3d291d9176e4d88a7b582c7665150c896297ff7
ALFIO BATTIATO <battiato3@gmail.com>
2024-04-05 09:38:53 +0200

    ok

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -70,11 +70,11 @@ function Home() {
                   role="none"
                   data-supported-dps="16x16"
                   fill="#000000"
-                  fill-opacity="0.9"
+                  fillOpacity="0.9"
                   width="16"
                   height="16"
                 >
-                  <path d="M13 4a3 3 0 00-3-3H3v14l5-4.5 5 4.5z" fill-opacity="0.9"></path>
+                  <path d="M13 4a3 3 0 00-3-3H3v14l5-4.5 5 4.5z" fillOpacity="0.9"></path>
                 </svg>
                 <span>I miei elementi</span>
               </div>
@@ -95,7 +95,7 @@ function Home() {
                       height={"50rem"}
                       className="rounded-circle object-fit-cover"
                     />
-                    <button type="button" class=" flex-grow-1 border rounded-pill text-start">
+                    <button type="button" className=" flex-grow-1 border rounded-pill text-start">
                       Avvia un post
                     </button>
                   </div>
@@ -110,13 +110,13 @@ function Home() {
                         role="none"
                         data-supported-dps="24x24"
                         fill="#000000"
-                        fill-opacity="0.9"
+                        fillOpacity="0.9"
                         width="24"
                         height="24"
                       >
                         <path
                           d="M19 4H5a3 3 0 00-3 3v10a3 3 0 003 3h14a3 3 0 003-3V7a3 3 0 00-3-3zm1 13a1 1 0 01-.29.71L16 14l-2 2-6-6-4 4V7a1 1 0 011-1h14a1 1 0 011 1zm-2-7a2 2 0 11-2-2 2 2 0 012 2z"
-                          fill-opacity="0.9"
+                          fillOpacity="0.9"
                         ></path>
                       </svg>
                       <h6>Contenuti multimediali</h6>
@@ -130,13 +130,13 @@ function Home() {
                         role="none"
                         data-supported-dps="24x24"
                         fill="#000000"
-                        fill-opacity="0.9"
+                        fillOpacity="0.9"
                         width="24"
                         height="24"
                       >
                         <path
                           d="M3 3v15c0 1.66 1.34 3 3 3h12c1.66 0 3-1.34 3-3V3H3zm13 1.75a1.25 1.25 0 110 2.5 1.25 1.25 0 010-2.5zm-8 0a1.25 1.25 0 110 2.5 1.25 1.25 0 010-2.5zM19 18c0 .55-.45 1-1 1H6c-.55 0-1-.45-1-1V9h14v9zM7 11h2v2H7v-2zm0 4h2v2H7v-2zm4-4h2v2h-2v-2zm0 4h2v2h-2v-2zm4-4h2v2h-2v-2zm0 4h2v2h-2v-2z"
-                          fill-opacity="0.9"
+                          fillOpacity="0.9"
                         ></path>
                       </svg>
                       <h6>Evento</h6>
@@ -150,13 +150,13 @@ function Home() {
                         role="none"
                         data-supported-dps="24x24"
                         fill="#000000"
-                        fill-opacity="0.9"
+                        fillOpacity="0.9"
                         width="24"
                         height="24"
                       >
                         <path
                           d="M21 3v2H3V3zm-6 6h6V7h-6zm0 4h6v-2h-6zm0 4h6v-2h-6zM3 21h18v-2H3zM13 7H3v10h10z"
-                          fill-opacity="0.9"
+                          fillOpacity="0.9"
                         ></path>
                       </svg>
                       <h6>Scrivi un articolo</h6>
@@ -164,11 +164,7 @@ function Home() {
                   </div>
                 </div>
 
-                {/* <div className="d-flex p-2 gap-2 flex-wrap border rounded bg-white">
-                  <MyButton text={"Tutto"} colore={"success"}></MyButton>
-                  <MyButton text={"I miei post"} colore={"outline-secondary"}></MyButton>
-                  <MyButton text={"Menzioni"} colore={"outline-secondary"}></MyButton>
-                </div> */}
+              
               </Col>
               <Col xs={12}>
                 <div className="rounded bg-white border p-3">
